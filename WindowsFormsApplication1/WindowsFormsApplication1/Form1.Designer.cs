8e98908c8415aebe97c7e81e778b910d35d875d7
Brandon Wright <brandonwright831@gmail.com>
2015-06-08 14:36:10 -0400

    Initial commit

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkinButton = new System.Windows.Forms.Button();
+            this.checkoutButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // checkinButton
+            // 
+            this.checkinButton.Location = new System.Drawing.Point(12, 166);
+            this.checkinButton.Name = "checkinButton";
+            this.checkinButton.Size = new System.Drawing.Size(75, 23);
+            this.checkinButton.TabIndex = 0;
+            this.checkinButton.Text = "Check-in";
+            this.checkinButton.UseVisualStyleBackColor = true;
+            this.checkinButton.Click += new System.EventHandler(this.checkinButton_Click);
+            // 
+            // checkoutButton
+            // 
+            this.checkoutButton.Location = new System.Drawing.Point(225, 166);
+            this.checkoutButton.Name = "checkoutButton";
+            this.checkoutButton.Size = new System.Drawing.Size(73, 24);
+            this.checkoutButton.TabIndex = 1;
+            this.checkoutButton.Text = "Check-out";
+            this.checkoutButton.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(313, 205);
+            this.Controls.Add(this.checkoutButton);
+            this.Controls.Add(this.checkinButton);
+            this.Name = "Choice Form";
+            this.Text = "DePauw SDS Equipment Tracker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button checkinButton;
+        private System.Windows.Forms.Button checkoutButton;
+    }
+}
+
